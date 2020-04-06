c059e504c20c85d2f174c56eb92df1ff06a12c4e
Philip Franchi-Pereira <philip.franchi-pereira@nytimes.com>
2020-04-05 23:21:05 -0400

    Added blog switching

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
 import Page from '../Page/Page'
+import './Blog.css'
+import {Link, Switch, Route, useRouteMatch, useParams} from 'react-router-dom'
+
+function BlogPost() {
+    let { postId } = useParams();
+    return <Page pageName={postId ? postId : ""}>
+            <p>Thus, we see a post called {postId}</p>
+        </Page>
+}
+
 export default function Blog() {
-    return <Page pageName="Blog">
-        
-    </Page>
+    let match = useRouteMatch();
+    return <Switch> 
+            <Route path={`${match.url}/:postId`}><BlogPost /></Route>
+            <Route path={match.url}> 
+                <Page pageName="Blog">
+                    <ul className="Blog">
+                        <li><Link to={`${match.url}/post1`}>Post 1</Link></li>
+                        <li><Link to={`${match.url}/post2`}>Post 2</Link></li>
+                        <li><Link to={`${match.url}/post3`}>Post 3</Link></li>
+                    </ul>
+                </Page>
+            </Route>
+        </Switch>
 }
